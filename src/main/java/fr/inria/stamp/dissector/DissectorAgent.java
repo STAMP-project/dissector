bc6d31232093766c4b0b39ca04579e90f40f3e7c
oscarlvp <oscarlvp@gmail.com>
2017-11-21 16:56:45 +0100

    A list of methods can be passed to filter the methods to be instrumented. This list should contain a method signature for each line. The signature should not include the return type. Is similar to the result obtained with getLongName in javassist. The line could contain a : separating the signature from extra information. That makes the input compatible with the agent's output.

--- src/main/java/fr/inria/stamp/dissector/DissectorAgent.java
+++ src/main/java/fr/inria/stamp/dissector/DissectorAgent.java
@@ -1,11 +1,17 @@
 package fr.inria.stamp.dissector;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.ProtectionDomain;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import fr.inria.stamp.dissector.StaticDatabase;
 import javassist.CtBehavior;
@@ -28,11 +34,42 @@ public class DissectorAgent {
             }
         }));
 
-        //TODO: Set the filter to a given set of methods, infer the classes from there
-        Predicate<CtBehavior> filter = (b) ->
-            b.getLongName().startsWith("fr.inria.stamp.dissector.test");
-        MethodTransformer transformer = new MethodTransformer(filter);
+        //TODO: Args validation
+        Set<String> methods = loadMethods(agentArgs);
+        MethodTransformer transformer = new MethodTransformer(
+                /*getClassPredicate(methods),*/
+                (aClass) -> true,
+                getMethodPredicate(methods));
         inst.addTransformer(transformer);
+    }
+
+    protected static Set<String> loadMethods(String path) {
+        try(BufferedReader reader = Files.newBufferedReader(Paths.get(path))){
+            return reader.lines()
+                    .map((str) ->  str.substring(0, str.indexOf(':')))
+                    .collect(Collectors.toSet());
+        }catch (IOException exc) {
+            //TODO: Improve
+            exc.printStackTrace();
+            throw new AssertionError("Failed to load the input file");
+        }
+    }
+
+    protected static Predicate<CtBehavior> getMethodPredicate(Set<String> methods) {
+        return (method) -> methods.contains(method.getLongName());
+    }
+
+    protected static Predicate<CtClass> getClassPredicate(Set<String> methods) {
+        //FIXME: Should be the last index of . before (
+        Set<String> classes = methods.stream()
+                                        .map((str) -> str.substring(str.indexOf('(')))
+                                        .collect(Collectors.toSet());
+
+        return (aClass) -> {
+            System.out.println(aClass.getName());
+           return classes.contains(aClass.getName());
+        };
+
     }
 
 }
